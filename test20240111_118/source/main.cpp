f6510d5ed5311939b0580472266c6bf9b92de54f
carlos99cc <13476575876@163.com>
2024-01-17 00:05:41 +0800

    1

--- test20240111_118/source/main.cpp
+++ test20240111_118/source/main.cpp
@@ -1,64 +1,26 @@
 #include<iostream>
 #include<string>
 
-using namespace std;
-class Building;
-class GoodGay
+using  namespace std;
+
+class Buildi
+class Godgay
 {
 public:
    Building *b;
-   /* data */
-   GoodGay(/* args */);
-   ~GoodGay();
+public:
+   Godgay(/* args */);
+   ~Godgay();
    void visit();
-};
-class Building
-{
-friend class GoodGay;
-public:
-string m_sittingroom;
-private:
-string m_bedroom;
-public:
-
-   Building()//类的构造函数
-   {
-      m_sittingroom="客厅";
-      m_bedroom="卧室";
-   }
-   ~Building()
-   {
-
-   }
-
+   void visit2();
 };
 
-//函数也可以类外写
-GoodGay::GoodGay(/* args */)
+Godgay::Godgay(/* args */)
 {
-   //创建建筑物的对象
-   b = new Building;//坐边的是Buuliding创建的对象指针，右边new的是一个Buliding类大小的堆内存，这个就是拷贝构造的隐式转换法
 }
 
-GoodGay::~GoodGay()
+Godgay::~Godgay()
 {
-   if (b != NULL)
-   {
-      /* code */
-      delete b;
-      b = NULL;
-   }
-   
-}
-void GoodGay::visit()
-{
-   cout<<"老朋友全局函数 正在访问 ："<<b->m_sittingroom<<endl;
-   cout<<"老朋友全局函数 正在访问 ："<<b->m_bedroom<<endl;
-}
-void test()
-{
-   GoodGay gg;
-   gg.visit();
 }
 
 int main()
